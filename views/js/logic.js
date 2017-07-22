7179c27913bba89cc38e0e2c2e8783b2d2d2a8f8
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-22 11:31:31 -0400

    database post

---
+++ views/js/logic.js
@@ -0,0 +1,31 @@
+$("#reserve").click(function(){
+	console.log("clicked");
+	var name = $("#nameField").val().trim();
+	var date = $("#dateField").val().trim();
+	var nights = $("#nightsField").val().trim();
+
+	var newReservation = {
+		name: name,
+		date: date,
+		nights: nights
+	};
+	$.post("/api/new/reservation", newReservation)
+		.done(function(data) {
+			console.log(data);
+		});
+});
+
+$("#comment").click(function() {
+	console.log("comment");
+	var name = $("#commentName").val().trim();
+	var comment = $("#commentArea").val();
+
+	var newComment = {
+		name: name,
+		comment: comment
+	};
+	$.post("/api/new/comment", newComment)
+		.done(function(data) {
+			console.log(data);
+		});
+});
